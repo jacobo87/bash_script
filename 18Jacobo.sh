9c763c02d9871defa3788f3a5e5059054c9daaf0
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2020-04-28 18:37:21 +0200

    correccion errores

--- 18Jacobo.sh
+++ 18Jacobo.sh
@@ -1,23 +1,18 @@
 #! /bin/bash
 clear
-#18.Crea un script al que se le pasen dos parámetros un directorio y un fichero.
-#Verificar la existencia de los parámetros, caso contrario mostrar un mensaje indicando que no se le pasaron dichos parámetros.
-#Comprobar que el fichero se encuentre en el directorio, en cuyo caso mostrará toda la información del fichero, caso contrario mostrará un mensaje.
-#! /bin/bash
-clear
 read -p "Escriba el nombre de un directorio: " directorio
 if [ -d $directorio ];
 then
     echo "Ha elegido el siguiente directorio válido: " $directorio
-    sleep 1s
+    #sleep 1s
     read -p "Ahora escriba el nombre de un fichero: " fichero
-    if [ -e $fichero ];
+    if [ -f $fichero ];
     then
         echo "Ha elegido el siguiente fichero válido: " $fichero
     else
         echo "El fichero escogido no es válido"
     fi
-    if [ pwd $fichero = pwd $directorio ];
+    if [ ?? ] ;
     then
         echo "$fichero está en el directorio."
     else
---
+++ 19Jacobo.sh
@@ -0,0 +1,9 @@
+#! bin/bash
+clear
+#Buscar los ficheros que contengan una serie de caracteres que el usuario introduzca por teclado, mostrando por pantalla todos los ficheros que los contengan.
+#El proceso se ejecutará hasta que el usuario introduzca un carácter determinado.
+until [[ $tecla = 1 tecla ]] 
+do
+echo "pulsa la tecla correcta" $tecla
+read n- 1 tecla
+done
